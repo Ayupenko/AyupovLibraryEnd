8ec0af089cfb01fbb9c0594fdafbb88f4f1182ac
Bulat <b.aiupov@yandex.ru>
2024-11-12 02:50:13 +0500

    Добавьте файлы проекта.

---
+++ AddEditPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AyupovLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private FundLibrary _currentLibrary = new FundLibrary();
+
+        public AddEditPage(FundLibrary SelectedLibrary)
+        {
+            InitializeComponent();
+
+            var FoundLib = LibraryEntities.GetContext().Libraries.Select(p => p.NameLibrary).ToList();
+
+            foreach (var FoundLibItem in FoundLib)
+            {
+                LibraryComboBox.Items.Add(FoundLibItem);
+            }
+
+
+
+            if (SelectedLibrary != null)
+            {
+                _currentLibrary = SelectedLibrary;
+                LibraryComboBox.SelectedIndex = _currentLibrary.LibraryID - 1;
+            }
+            else
+            {
+                LibraryComboBox.SelectedIndex = 0;
+            }
+
+
+
+
+            DataContext = _currentLibrary;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            StringBuilder errors= new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentLibrary.FundName))
+            {
+                errors.AppendLine("Укажите название фонда");
+            }
+          
+            if (_currentLibrary.QuanBooks <= 0 || _currentLibrary.QuanBooks.GetType() != typeof(int))
+            {
+                errors.AppendLine("Укажите количество книг");
+            }
+            if (_currentLibrary.QuanJournals <= 0 || _currentLibrary.QuanJournals.GetType() != typeof(int))
+            {
+                errors.AppendLine("Укажите количество журналов");
+            }
+            if (_currentLibrary.QuanNewspapers <= 0 || _currentLibrary.QuanNewspapers.GetType() != typeof(int))
+            {
+                errors.AppendLine("Укажите количество газет");
+            }
+            if (_currentLibrary.QuanDissertations <= 0 || _currentLibrary.QuanDissertations.GetType() != typeof(int))
+            {
+                errors.AppendLine("Укажите количество диссертаций");
+            }
+            if (_currentLibrary.QuanReferats <= 0 || _currentLibrary.QuanReferats.GetType() != typeof(int))
+            {
+                errors.AppendLine("Укажите количество рефератов");
+            }
+            if (_currentLibrary.QuanCollections <= 0 || _currentLibrary.QuanCollections.GetType() != typeof(int))
+            {
+                errors.AppendLine("Укажите количество Коллекционных");
+            }
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentLibrary.FundID == 0)
+                LibraryEntities.GetContext().FundLibrary.Add(_currentLibrary);
+
+            try
+            {
+                LibraryEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+               
+        }
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                _currentLibrary.MainImagePath = myOpenFileDialog.FileName;
+                LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+            }
+        }
+    }
+}
---
+++ LibraryPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AyupovLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для LibraryPage.xaml
+    /// </summary>
+    public partial class LibraryPage : Page
+    {
+       
+        public LibraryPage()
+        {
+           
+            InitializeComponent();
+
+            var currentLibrary = LibraryEntities.GetContext().FundLibrary.ToList();
+
+            LibraryListView.ItemsSource = currentLibrary;
+            ComboType.SelectedIndex=0;
+
+            List<FundLibrary> currentLibrarys = LibraryEntities.GetContext().FundLibrary.ToList();
+
+            allLibraryCount.Text = currentLibrarys.Count().ToString();
+
+            LibraryListView.ItemsSource = currentLibrarys;
+
+            UpdateLibrary();
+
+        }
+
+        private void UpdateLibrary()
+        {
+            var currentLibrary=LibraryEntities.GetContext().FundLibrary.ToList();
+            if(ComboType.SelectedIndex==0) 
+            {
+            
+            
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentLibrary=currentLibrary.Where(p=>(p.QuanBooks >=0 && p.QuanBooks <=10)).ToList();
+
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentLibrary = currentLibrary.Where(p => (p.QuanBooks >= 10 && p.QuanBooks <= 20)).ToList();
+
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentLibrary = currentLibrary.Where(p => (p.QuanBooks >= 20 && p.QuanBooks <= 30)).ToList();
+
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentLibrary = currentLibrary.Where(p => (p.QuanBooks >= 30)).ToList();
+
+            }
+
+            currentLibrary = currentLibrary.Where(p=>p.FundName.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Libraries.NameLibrary.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if(RButtonDown.IsChecked.Value)
+            {
+                currentLibrary=currentLibrary.OrderByDescending(p=>p.FundName).ToList();
+            }
+            if (RButtonUP.IsChecked.Value)
+            {
+                currentLibrary = currentLibrary.OrderBy(p => p.FundName).ToList();
+            }
+            
+            currentLibraryCount.Text = currentLibrary.Count().ToString();
+
+            LibraryListView.ItemsSource = currentLibrary;
+
+            LibraryListView.ItemsSource=currentLibrary;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateLibrary();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateLibrary();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RButtonUP_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateLibrary();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateLibrary();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as FundLibrary));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                LibraryEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                LibraryListView.ItemsSource=LibraryEntities.GetContext().FundLibrary.ToList();
+            }
+        }
+
+       
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentLibrary=(sender as Button).DataContext as FundLibrary;
+            if(MessageBox.Show("Вы точно хотите выполнить удаление?","Внимание!", MessageBoxButton.YesNo,MessageBoxImage.Question)
+                ==MessageBoxResult.Yes)
+            {
+                try
+                {
+                    LibraryEntities.GetContext().FundLibrary.Remove(currentLibrary);
+                    LibraryEntities.GetContext().SaveChanges();
+
+                    LibraryListView.ItemsSource = LibraryEntities.GetContext().FundLibrary.ToList();
+
+                    UpdateLibrary();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
